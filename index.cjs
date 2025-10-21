37ee4de3db2ebf8e650ee2d7141e44fcae4e9d0f
Shayan <shayan.cs11@gmail.com>
2025-10-21 15:18:56 +0500

    fix:dotenv in cjs

--- index.cjs
+++ index.cjs
@@ -1,9 +1,11 @@
 "use strict";
 
+require("dotenv").config();
+
 // CJS shim that lazy-loads the ESM module.
 // Works on modern Node without extra deps.
 exports.initLLM = (...args) =>
   import("./index.esm.js").then(m => m.initLLM(...args));
-
+b
 exports.useLLM = (...args) =>
   import("./index.esm.js").then(m => m.useLLM(...args));
